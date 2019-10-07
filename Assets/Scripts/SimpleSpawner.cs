a27fc5bf0d9a5ab97b54315e11ecd0a29de03717
Feranmi <oladosuferanmi@gmail.com>
2019-10-07 22:28:02 +0100

    Initiali Commit

---
+++ Assets/Scripts/SimpleSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSpawner : MonoBehaviour
+{
+    public GameObject ball;
+    public Transform spawnPoint;
+    public float speed = 800f;
+
+    private void Start()
+    {
+        InvokeRepeating("Fire", .1f, 1);
+    }
+
+    public void Fire()
+    {
+        GameObject b = Instantiate(ball, spawnPoint.position, Quaternion.identity);
+        b.GetComponent<Rigidbody>().AddForce(speed * Time.deltaTime * -Vector3.forward);
+    }
+}
